6f935cbf0f115fe79dd81c9247a94d48ab349589
jrfep <jr.ferrer.paris@gmail.com>
2019-10-04 08:14:36 +1000

    spatial data needed to calculate bounding box of macrogroups, #9 and #10

--- env/roraima.sh
+++ env/roraima.sh
@@ -1,2 +1,3 @@
 export WORKDIR=$HOME/tmp/Macrogroups_Americas
 export SCRIPTDIR=$HOME/proyectos/IUCN/RLE-Americas-Forest-Macrogroups
+export GISDATA=$HOME/gisdata
---
+++ inc/grass/create-IVC-location.sh
@@ -0,0 +1,5 @@
+grass -c IVC_NS_v7_270m_robin $GISDB/IVC
+
+#import raster_data
+# create lat and long raster
+# cross IVC with lat and long and extract extremes for bounding box
