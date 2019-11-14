23bf13b455f3790891ff4e8a73bf6c71c655e9f1
jrfep <jr.ferrer.paris@gmail.com>
2019-11-14 11:38:21 +1100

    advance xml structure, criteria

---
+++ inc/R/xml-generator/create-subcriteria-E-node.R
@@ -0,0 +1,5 @@
+
+newXMLNode("Criterion",attrs=list(name="E"),
+  children=list(newXMLNode("Summaries",children=list(newXMLNode("Summary",attrs=list(lang="en"),"No quantitative assessment of ecosystem collapse was performed for this assessment unit."))),
+  newXMLNode("Rationale",attrs=list(name="E"),"This criterion was not evaluated."),
+  newXMLNode("Category","NE")))
