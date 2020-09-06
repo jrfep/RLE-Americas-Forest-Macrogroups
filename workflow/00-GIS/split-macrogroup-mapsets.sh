fee1ce99871e7fac6bf26eec6b697eb493881e9b
jrfep <jr.ferrer.paris@gmail.com>
2020-09-07 07:41:24 +1000

    ifelse

--- workflow/00-GIS/split-macrogroup-mapsets.sh
+++ workflow/00-GIS/split-macrogroup-mapsets.sh
@@ -7,7 +7,17 @@ do
   export CDG=$(echo $k | cut -d"|" -f 1)
   g.mapset -c $MCDG
   g.region rast=IVC_NS_v7
-  test $(g.list type=raster pattern=$MCDG | wc -l) == 1  && echo "$MCDG raster listo" || r.mapcalc ${MCDG}'=if(IVC_NS_v7=='${CDG}',1,null())'
-  test $(g.list type=vector pattern=$MCDG | wc -l) == 1  && echo "$MCDG vector listo" || r.to.vect -v input=${MCDG} output=${MCDG} type=area
+  if [ $(g.list type=raster pattern=$MCDG | wc -l) == 1 ]
+  then
+     echo "$MCDG raster listo"
+  else
+    r.mapcalc ${MCDG}'=if(IVC_NS_v7=='${CDG}',1,null())'
+  fi
+  if   [ $(g.list type=vector pattern=$MCDG | wc -l) == 1 ]
+  then
+    echo "$MCDG vector listo"
+  else
+     r.to.vect -v input=${MCDG} output=${MCDG} type=area
+  fi
   g.region vect=${MCDG}
 done
